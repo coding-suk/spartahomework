f78375996b93abd7e85fdfc5e9a3b2758fc4c486
coding-suk <141230562+coding-suk@users.noreply.github.com>
2024-07-25 10:02:44 +0900

    Level1 3번까지 수행완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,36 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int firNum, secNum;
+        char sign;
+        int result = 0;
 
+        firNum = sc.nextInt();
+        System.out.println("첫 번째 숫자를 입력하세요: " +firNum);
+
+        secNum = sc.nextInt();
+        System.out.println("두 번쨰 숫자를 입력하세요: " +secNum);
+
+        sign = sc.next().charAt(0);
+        System.out.println("사칙연산 기호를 입력하세요: " +sign);
+
+        if (sign=='+') {
+            result = firNum + secNum;
+        } else if (sign == '-') {
+            result = firNum - secNum;
+        } else if (sign == '*') {
+            result = firNum * secNum;
+        } else if ( sign == '/') {
+            if (secNum != 0) {
+                result = firNum / secNum;
+            } else {
+                System.out.println("분모에는 0이 들어갈수 없습니다.");
+            }
+        }
     }
 }
