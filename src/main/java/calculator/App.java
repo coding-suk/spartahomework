03105f6e94bda887b20176ef25c6b9220b6baf69
coding-suk <141230562+coding-suk@users.noreply.github.com>
2024-07-25 20:32:32 +0900

    Level1 5번까지 수행완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,29 +7,46 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int firNum, secNum;
+        String s;
         char sign;
         int result = 0;
+        int[] intarr = new int[10];
+        int count =0;
 
-        firNum = sc.nextInt();
-        System.out.println("첫 번째 숫자를 입력하세요: " +firNum);
+        while (true) {
+            firNum = sc.nextInt();
+            System.out.println("첫 번째 숫자를 입력하세요: " + firNum);
 
-        secNum = sc.nextInt();
-        System.out.println("두 번쨰 숫자를 입력하세요: " +secNum);
+            secNum = sc.nextInt();
+            System.out.println("두 번쨰 숫자를 입력하세요: " + secNum);
 
-        sign = sc.next().charAt(0);
-        System.out.println("사칙연산 기호를 입력하세요: " +sign);
+            sign = sc.next().charAt(0);
+            System.out.println("사칙연산 기호를 입력하세요: " + sign);
 
-        if (sign=='+') {
-            result = firNum + secNum;
-        } else if (sign == '-') {
-            result = firNum - secNum;
-        } else if (sign == '*') {
-            result = firNum * secNum;
-        } else if ( sign == '/') {
-            if (secNum != 0) {
-                result = firNum / secNum;
-            } else {
-                System.out.println("분모에는 0이 들어갈수 없습니다.");
+            if (sign == '+') {
+                result = firNum + secNum;
+            } else if (sign == '-') {
+                result = firNum - secNum;
+            } else if (sign == '*') {
+                result = firNum * secNum;
+            } else if (sign == '/') {
+                if (secNum != 0) {
+                    result = firNum / secNum;
+                } else {
+                    System.out.println("분모에는 0이 들어갈수 없습니다.");
+                }
+            }
+            System.out.println("결과: " + result);
+            for(int i =0;i<intarr.length;i++) {
+                intarr[i] = result;
+                count++;
+            }
+
+
+            System.out.println("더 계산하기겠습니까? (exit 입력 시 종료)");
+            String sc1 = sc.next();
+            if (sc1.equals("exit")) {
+                break;
             }
         }
     }
